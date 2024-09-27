9dab5ee955beebc3c496d69f0aeac42396f83e88
harinia11 <harinia1110@gmail.com>
2024-09-27 10:06:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Rendering from "./Rendering-work/Rendering";
+
+function App() {
+  return (
+    <>
+    <Rendering/>
+    </>
+  )
+}
+export default App;
---
+++ src/Rendering-work/Rendering.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Rendering(){
+    return(
+        <>
+        <h1>HARINI</h1>
+        </>
+    )
+}
+export default Rendering;
